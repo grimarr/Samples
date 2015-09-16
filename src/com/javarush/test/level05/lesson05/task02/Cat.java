6ce46109a080d4fd76707bd3fd981871932f8b44
grimarr <frossen@techie.com>
2015-09-16 22:48:42 +0600

    Update class

--- src/com/javarush/test/level05/lesson05/task02/Cat.java
+++ src/com/javarush/test/level05/lesson05/task02/Cat.java
@@ -11,23 +11,47 @@ package com.javarush.test.level05.lesson05.task02;
 
 public class Cat
 {
-    public String name;
-    public int age;
-    public int weight;
-    public int strength;
-
-    public Cat()
-    {
+    private String name;
+    private int age;
+    private int weight;
+    private int strength;
+	public static int counter;
+	
+    public Cat(String name, int age, int weight, int strength) {
+		counter++;
+		this.name = name;
+		this.age = age;
+		this.weight = weight;
+		this.strength = strength;
     }
+	public Cat() {
+	}
+	
+	public String getName()	{return name;}
+	public int getAge()	{return age;}
+	public int getWeight()	{return weight;}
+    public int getStrength() {return strength;}
+	public void setName(String name) {this.name = name;}
+	public void  setAge(int age) {this.age = age;}
+	public void setWeight(int weight) {this.weight = weight;}
+	public void setStrength(int strength) {this.strength = strength;}
 
     public boolean fight(Cat anotherCat)
     {
-        if ((this.age < anotherCat.age)  && (this.weight > anotherCat.weight) || (this.strength > anotherCat.strength))
-        return true;
-        else
-            return false;
+        //if ((this.age < anotherCat.age)  && (this.weight > anotherCat.weight) || (this.strength > anotherCat.strength))
+        //return true;
+        //else
+        //    return false;
 
-        //напишите тут ваш код
-
+         return ((this.age < anotherCat.age) && (this.weight > anotherCat.weight) || (this.strength > anotherCat.strength));
+		
+		/*  Еще один вариант
+		
+			int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
+            int score = agePlus + weightPlus + strengthPlus;
+            return score > 2; // return score > 2 ? true : false;
+		*/
     }
 }

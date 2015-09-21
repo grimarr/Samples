50bd4b6a8b5d2167b365656660155be8fa990dc9
grimarr <frossen@techie.com>
2015-09-21 14:54:11 +0600

    reduction to a common view, try #1

--- src/com/javarush/test/level05/lesson12/bonus02/Solution.java
+++ src/com/javarush/test/level05/lesson12/bonus02/Solution.java
@@ -14,25 +14,19 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
-        int b = Integer.parseInt(reader.readLine());
-        int c = Integer.parseInt(reader.readLine());
-        int d = Integer.parseInt(reader.readLine());
-        int e = Integer.parseInt(reader.readLine());
-
-
-        int minimum = min(a, b, c, d, e);
-
-        System.out.println("Minimum = " + minimum);
+        int minimum;
+		int a = minimum = Integer.parseInt(reader.readLine());
+		for (int i = 1; i < 5; i++)
+		{
+        a = Integer.parseInt(reader.readLine());
+		minimum = min(minimum, a);
+        }
+		System.out.println("Minimum = " + minimum);
     }
 
-
-    public static int min(int a, int b, int c, int d, int e)
+    public static int min(int a, int b)
     {
-        return a < b && a < c && a < d && a < e ? a :
-                b < a && b < c && b < d && b < e ? b :
-                        c < a && c < b && c < d && c < e ? c :
-                                d < a && d < b && d < c && d < e ? d : e;
-    }
+		return a < b ? a : b;
+	}
 
 }

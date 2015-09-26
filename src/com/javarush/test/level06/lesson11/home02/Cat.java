fc67274ab13bafae2dc75d7d88ec319fadd0f835
grimarr <frossen@techie.com>
2015-09-26 14:37:30 +0600

    lesson 11

---
+++ src/com/javarush/test/level06/lesson11/home02/Cat.java
@@ -0,0 +1,33 @@
+package com.javarush.test.level06.lesson11.home02;
+
+import java.util.ArrayList;
+
+/* Статические коты
+1. В классе Cat добавь public статическую переменную cats (ArrayList<Cat>).
+2. Пусть при каждом создании кота (нового объекта Cat) в переменную cats добавляется этот новый кот. Создать 10 объектов Cat.
+3. Метод printCats должен выводить всех котов на экран. Нужно использовать переменную cats.
+*/
+
+public class Cat {
+
+    public Cat() {
+        Cat.cats.add(this);
+    }
+    public static ArrayList<Cat> cats = new ArrayList<Cat>();
+
+    public static void main(String[] args) {
+
+        while (Cat.cats.size() < 10)
+        {
+            new Cat();
+        }
+        printCats();
+    }
+
+    public static void printCats() {
+        for (int i = 0; i < cats.size(); i++)
+        {
+            System.out.print(cats.get(i) + " ");
+        }
+    }
+}

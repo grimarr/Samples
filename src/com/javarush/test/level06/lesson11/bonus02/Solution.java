b755b8420cb14458d42ebf3975b6b0d09f5101c3
grimarr <frossen@techie.com>
2015-09-26 14:38:21 +0600

    lesson 11, not working

---
+++ src/com/javarush/test/level06/lesson11/bonus02/Solution.java
@@ -0,0 +1,90 @@
+package com.javarush.test.level06.lesson11.bonus02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* Нужно добавить в программу новую функциональность
+Задача: У каждой кошки есть имя и кошка-мама. Создать класс, который бы описывал данную ситуацию.
+Создать два объекта: кошку-дочь и кошку-маму. Вывести их на экран.
+Новая задача: У каждой кошки есть имя, кошка-папа и кошка-мама.
+Изменить класс Cat так, чтобы он мог описать данную ситуацию.
+Создать 6 объектов: маму, папу, сына, дочь, бабушку(мамина мама) и дедушку(папин папа).
+Вывести их всех на экран в порядке: дедушка, бабушка, папа, мама, сын, дочь.
+
+Пример ввода:
+дедушка Вася
+бабушка Мурка
+папа Котофей
+мама Василиса
+сын Мурчик
+дочь Пушинка
+
+Пример вывода:
+Cat name is дедушка Вася, no mother, no father
+Cat name is бабушка Мурка, no mother, no father
+Cat name is папа Котофей, no mother, father is дедушка Вася
+Cat name is мама Василиса, mother is бабушка Мурка, no father
+Cat name is сын Мурчик, mother is мама Василиса, father is папа Котофей
+Cat name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
+*/
+
+// НЕ РАБОТАЕТ ПОЧЕМУ-ТО
+
+
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        Cat catGrandfather = new Cat(reader.readLine());         // Cat name is дедушка Вася, no mother, no father
+        Cat catGrandmother = new Cat(reader.readLine());         // Cat name is бабушка Мурка, no mother, no father
+        Cat catFather = new Cat(reader.readLine(),null, catGrandfather);    // Cat name is папа Котофей, no mother, father is дедушка Вася
+        Cat catMother = new Cat(reader.readLine(),catGrandmother, null);    // Cat name is мама Василиса, mother is бабушка Мурка, no father
+        Cat catSon = new Cat(reader.readLine(),catMother, catFather);    // Cat name is сын Мурчик, mother is мама Василиса, father is папа Котофей
+        Cat catDaughter = new Cat(reader.readLine(),catMother, catFather); // Cat name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
+
+        System.out.println(catGrandfather);
+        System.out.println(catGrandmother);
+        System.out.println(catFather);
+        System.out.println(catMother);
+        System.out.println(catSon);
+        System.out.println(catDaughter);
+    }
+
+    public static class Cat
+    {
+        private String name;
+        private Cat mother;
+        private Cat father;
+
+        Cat(String name)
+        {
+            this.name = name;
+        }
+
+        Cat(String name, Cat mother, Cat father)
+        {
+            this.name = name;
+            this.mother = mother;
+            this.father = father;
+        }
+
+        @Override
+        public String toString()    //Как еще можно написать проверку?
+        {
+            if ((mother == null) && (father == null))
+                return "Cat name is " + name + ", no mother" + ", no father";
+            else
+            if (mother == null)
+                return "Cat name is " + name + ", no mother, father is " + father.name;
+            else
+            if (father == null)
+                return "Cat name is " + name + ", mother is " + mother.name + ", no father";
+            else
+                return "Cat name is " + name + ", mother is " + mother.name + ", father is " + father.name;
+        }
+    }
+}

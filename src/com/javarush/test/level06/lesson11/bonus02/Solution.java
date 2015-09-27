27cd92621168c5622578d36aeced3e6373e07654
grimarr <frossen@techie.com>
2015-09-27 12:01:15 +0600

    fixed constructor Cat

--- src/com/javarush/test/level06/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level06/lesson11/bonus02/Solution.java
@@ -29,9 +29,6 @@ Cat name is сын Мурчик, mother is мама Василиса, father is 
 Cat name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
 */
 
-// НЕ РАБОТАЕТ ПОЧЕМУ-ТО
-
-
 
 public class Solution
 {
@@ -39,12 +36,12 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Cat catGrandfather = new Cat(reader.readLine());         // Cat name is дедушка Вася, no mother, no father
-        Cat catGrandmother = new Cat(reader.readLine());         // Cat name is бабушка Мурка, no mother, no father
-        Cat catFather = new Cat(reader.readLine(),null, catGrandfather);    // Cat name is папа Котофей, no mother, father is дедушка Вася
-        Cat catMother = new Cat(reader.readLine(),catGrandmother, null);    // Cat name is мама Василиса, mother is бабушка Мурка, no father
-        Cat catSon = new Cat(reader.readLine(),catMother, catFather);    // Cat name is сын Мурчик, mother is мама Василиса, father is папа Котофей
-        Cat catDaughter = new Cat(reader.readLine(),catMother, catFather); // Cat name is дочь Пушинка, mother is мама Василиса, father is папа Котофей
+        Cat catGrandfather = new Cat(reader.readLine());
+        Cat catGrandmother = new Cat(reader.readLine());
+        Cat catFather = new Cat(reader.readLine(), catGrandfather, null);
+        Cat catMother = new Cat(reader.readLine(), null, catGrandmother);
+        Cat catSon = new Cat(reader.readLine(),catFather, catMother);
+        Cat catDaughter = new Cat(reader.readLine(), catFather, catMother);
 
         System.out.println(catGrandfather);
         System.out.println(catGrandmother);
@@ -65,7 +62,7 @@ public class Solution
             this.name = name;
         }
 
-        Cat(String name, Cat mother, Cat father)
+        Cat(String name, Cat father, Cat mother) // Правильный порядок ввода данных
         {
             this.name = name;
             this.mother = mother;

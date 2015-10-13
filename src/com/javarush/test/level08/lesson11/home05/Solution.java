be6d95ffd24243d5baa27f4dced7939bcf35549b
grimarr <frossen@techie.com>
2015-10-13 12:29:16 +0600

    wrong solution lesson11\home5

--- src/com/javarush/test/level08/lesson11/home05/Solution.java
+++ src/com/javarush/test/level08/lesson11/home05/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.test.level08.lesson11.home05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /* Мама Мыла Раму. Теперь с большой буквы
 Написать программу, которая вводит с клавиатуры строку текста.
@@ -22,21 +24,24 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
-
         char[] aS = s.toCharArray();
-        if (Character.isAlphabetic(aS[0])) {
-            aS[0] = Character.toUpperCase(aS[0]);
-            for (int i = 1; i < aS.length; i++) {
-                if (Character.isAlphabetic(aS[i]) && !Character.isAlphabetic(aS[i - 1]))
-                    aS[i] =  Character.toUpperCase(aS[i]);
-            }
-        }
-        else
-            for (int i = 1; i < aS.length; i++) {
-                if (Character.isAlphabetic(aS[i]) && !Character.isAlphabetic(aS[i - 1]))
-                    aS[i] =   Character.toUpperCase(aS[i]);
-            }
+            outer:for (int i = 0; i < aS.length;)
+                if (!Character.isWhitespace(aS[i])){
+                        aS[i] = Character.toUpperCase(aS[i]);
 
+                        for (int o = i; o < aS.length;)
+                            if (!Character.isWhitespace(aS[o]))
+                                o++;
+                            else {
+                                i = o;
+                                continue outer;
+                            }
+                }
+                else i++;
         System.out.println(new String (aS));
+
     }
 }
+
+
+

fcebb41881b2ccb6e40e2df169f67133efe8a42e
grimarr <frossen@techie.com>
2015-10-14 16:26:12 +0600

    solved lvl09 less2 StackTrace

---
+++ src/com/javarush/test/level09/lesson02/task03/Solution.java
@@ -0,0 +1,49 @@
+package com.javarush.test.level09.lesson02.task03;
+
+/* Метод должен вернуть номер строки кода, из которого вызвали этот метод
+Написать пять методов, которые вызывают друг друга.
+Метод должен вернуть номер строки кода, из которого вызвали этот метод. 
+Воспользуйся функцией: element.getLineNumber().
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        method1();
+    }
+
+    public static int method1()
+    {
+        method2();
+		StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        return  element[2].getLineNumber(); // 2 - Информация о методе который вызвал метод с обращением к стеку
+    }
+
+    public static int method2()
+    {
+        method3();
+		StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        return  element[2].getLineNumber();
+    }
+
+    public static int method3()
+    {
+        method4();
+		StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        return  element[2].getLineNumber();
+    }
+
+    public static int method4()
+    {
+        method5();
+		StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        return  element[2].getLineNumber();
+    }
+
+    public static int method5()
+    {
+        StackTraceElement[] element = Thread.currentThread().getStackTrace();
+        return  element[2].getLineNumber();
+    }
+}

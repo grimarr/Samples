f12001b7c13b05ee883f5cb66111b985961e9ac3
grimarr <frossen@techie.com>
2015-10-25 19:17:42 +0600

    lvl10 stupid hashmap

--- src/com/javarush/test/level10/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level10/lesson11/bonus02/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.test.level10.lesson11.bonus02;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,47 +29,19 @@ public class Solution
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, ArrayList<String>> mapa = new HashMap<Integer, ArrayList<String>>();
+        Map<String, Integer> mapa = new HashMap<String, Integer>();
 
-        //запускаем бесконечный цикл ввода пар
         for (;;) {
             String first = reader.readLine(); //ввод числа и проверка на пустоту
             if (first.isEmpty())
                 break;
             String name = reader.readLine();
             int id = Integer.parseInt(first);
-
-            // заполнение мапы
-            if (!mapa.containsKey(id)) {
-                ArrayList<String> ls = new ArrayList<String>();
-                ls.add(name);
-                mapa.put(id, ls);
-            }
-            else{
-                ArrayList<String> ls = mapa.get(id);
-
-                // проверка на уникальность строки
-                if (!ls.contains(name)) {
-                    ls.add(name);
-                    mapa.put(id, ls);
-                }
-            }
+            mapa.put(name, id);
         }
 
-        // поиск самого длинного списка строк
-        int i = 0;
-        for (Map.Entry<Integer, ArrayList<String>> pair : mapa.entrySet())
-            if (pair.getValue().size() > i)
-                i = pair.getValue().size();
-
-        // вывод мапы
-        for (int x = 0;x < i; x++) {
-            for (Map.Entry<Integer, ArrayList<String>> pair : mapa.entrySet()) {
-                if (!pair.getValue().isEmpty()) {
-                    System.out.println(pair.getKey() + " " + pair.getValue().get(0));
-                    pair.getValue().remove(0);
-                }
-            }
+        for (Map.Entry<String, Integer> pair : mapa.entrySet()) {
+            System.out.println(pair.getValue() + " " + pair.getKey());
         }
     }
 }
